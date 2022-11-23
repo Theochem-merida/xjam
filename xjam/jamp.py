add79a79ab2387c0d0de6d5a79b5aaedb17dce66
Theochem Merida <108087193+Theochem-merida@users.noreply.github.com>
2022-11-23 13:22:02 -0600

    Add files via upload

--- xjam/jamp.py
+++ xjam/jamp.py
@@ -12,18 +12,18 @@ def allatomsx(a,atom_list):
     allatoms=list(zip(atom_list,[cj,ck]))
     return allatoms
 #-------------------------------------
-def poscardata(a,zdlist,zlattice,buckling):
+def poscardata(a, zdlist, zlattice, buckling):
     listaxyz=[]
     for i in range(a.shape[0]):
         for j in range(a.shape[1]):
             ichar=split(a[i,j])
             signo,letra=ichar[0],ichar[1]
             if letra != '0':
-                if j==0: xd,yd=0.0000000000,0.0000000000
-                if j==1: xd,yd=0.3333333333,0.6666666667
-                if j==2: xd,yd=0.6666666667,0.3333333333
-                if signo=='-': zd=zdlist[i]-(buckling/zlattice)
-                if signo=='+': zd=zdlist[i]+(buckling/zlattice)
+                if   j==0: xd,yd=0.0000000000,0.0000000000
+                elif j==1: xd,yd=0.3333333333,0.6666666667
+                elif j==2: xd,yd=0.6666666667,0.3333333333
+                if   signo=='-': zd=zdlist[i] - buckling/(2.0*zlattice)
+                elif signo=='+': zd=zdlist[i] + buckling/(2.0*zlattice)
                 if   letra == 'j': zi=0
                 elif letra == 'k': zi=1
                 listaxyz.append([xd, yd, zd, zi])
@@ -31,12 +31,12 @@ def poscardata(a,zdlist,zlattice,buckling):
     return listaxdydzd
 #-------------------------------------
 def build_poscarx(a,inamex,filename,z_vacuum,num_layers,d,latticep,atom_list,buckling,flag='w'):
-    zlattice=(z_vacuum+float(num_layers-1)*d)
-    zmax = (zlattice+float(num_layers-1)*d)/2.0
+    zaux=float(num_layers - 1)*d
+    zlattice=z_vacuum + zaux
+    zmax = 0.5 + float(num_layers - 1)*(d+buckling)/(2.0*zlattice)
     zdlist=[]
     for ii in range(num_layers):
-        zc=zmax-float(ii)*d
-        zd=zc/zlattice
+        zd=zmax-float(ii)*(d+buckling)/zlattice
         zdlist.append(zd)
     fopen = open(filename,flag)
     print("%s" %(inamex), file=fopen)
